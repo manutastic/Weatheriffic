49451b2557efae29bc7688396011442407d9f0c2
Manu O <manutastic@gmail.com>
2018-03-24 16:37:15 -0400

    add current location services

---
+++ app/src/main/java/com/manutastic/weatheriffic/GPSFunctions.java
@@ -0,0 +1,16 @@
+package com.manutastic.weatheriffic;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import static com.manutastic.weatheriffic.WeatherFunctions.mContext;
+
+/**
+ * Created by manut on 3/13/2018.
+ */
+
+public class GPSFunctions {
+}
--- app/src/main/java/com/manutastic/weatheriffic/MainActivity.java
+++ app/src/main/java/com/manutastic/weatheriffic/MainActivity.java
@@ -1,17 +1,30 @@
 package com.manutastic.weatheriffic;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import static com.manutastic.weatheriffic.WeatherFunctions.mContext;
 
@@ -23,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
             sunrise_field, sunset_field, humidity_field, pressure_field, wind_field,
             wind_dir_field;
     ImageView condition_icon_image;
+
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 1;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,25 +61,33 @@ public class MainActivity extends AppCompatActivity {
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        updateWeather();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         updateWeather();
     }
 
     public void updateWeather() {
-        current_temp_field = (TextView)findViewById(R.id.current_temp);
-        location_field = (TextView)findViewById(R.id.location);
-        high_temp_field = (TextView)findViewById(R.id.high_temp);
-        low_temp_field = (TextView)findViewById(R.id.low_temp);
-        condition_field = (TextView)findViewById(R.id.condition);
-        condition_icon_image = (ImageView)findViewById(R.id.condition_icon);
-        sunrise_field = (TextView)findViewById(R.id.sunrise);
-        sunset_field = (TextView)findViewById(R.id.sunset);
-        humidity_field = (TextView)findViewById(R.id.humidity);
-        pressure_field = (TextView)findViewById(R.id.pressure);
-        wind_field = (TextView)findViewById(R.id.wind);
-        wind_dir_field = (TextView)findViewById(R.id.wind_dir);
-        condition_icon_image = (ImageView)findViewById(R.id.condition_icon);
+        current_temp_field = (TextView) findViewById(R.id.current_temp);
+        location_field = (TextView) findViewById(R.id.location);
+        high_temp_field = (TextView) findViewById(R.id.high_temp);
+        low_temp_field = (TextView) findViewById(R.id.low_temp);
+        condition_field = (TextView) findViewById(R.id.condition);
+        condition_icon_image = (ImageView) findViewById(R.id.condition_icon);
+        sunrise_field = (TextView) findViewById(R.id.sunrise);
+        sunset_field = (TextView) findViewById(R.id.sunset);
+        humidity_field = (TextView) findViewById(R.id.humidity);
+        pressure_field = (TextView) findViewById(R.id.pressure);
+        wind_field = (TextView) findViewById(R.id.wind);
+        wind_dir_field = (TextView) findViewById(R.id.wind_dir);
+        condition_icon_image = (ImageView) findViewById(R.id.condition_icon);
 
-        WeatherFunctions.placeIdTask asyncTask = new WeatherFunctions.placeIdTask(new WeatherFunctions.AsyncResponse(){
+        final WeatherFunctions.placeIdTask asyncTask = new WeatherFunctions.placeIdTask(new WeatherFunctions.AsyncResponse() {
             public void processFinish(String current_temp, String location, String high_temp, String low_temp, String condition,
                                       String sunrise, String sunset, String humidity, String pressure, String wind,
                                       String wind_dir, int weather_icon) {
@@ -81,7 +105,33 @@ public class MainActivity extends AppCompatActivity {
                 condition_icon_image.setImageResource(weather_icon);
             }
         });
-        asyncTask.execute("55.7558", "37.6173", units); // Latitude N, Longitude E;
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                // Request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSION_ACCESS_FINE_LOCATION);
+
+                updateWeather();
+
+        } else {
+            // Permission has been granted
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                asyncTask.execute("" + location.getLatitude(), "" + location.getLongitude(), units); // Latitude N, Longitude E;
+                            } else {
+                                Toast.makeText(mContext, "Unable to get location", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
     }
 
     @Override
@@ -92,13 +142,13 @@ public class MainActivity extends AppCompatActivity {
                 if (units == "imperial") {
                     units = "metric";
                     item.setTitle(getResources().getString(R.string.c));
-                    TextView wind_units = (TextView)findViewById(R.id.wind_units);
+                    TextView wind_units = (TextView) findViewById(R.id.wind_units);
                     wind_units.setText(getResources().getString(R.string.wind_units_metric));
                     updateWeather();
                 } else if (units == "metric") {
                     units = "imperial";
                     item.setTitle(getResources().getString(R.string.f));
-                    TextView wind_units = (TextView)findViewById(R.id.wind_units);
+                    TextView wind_units = (TextView) findViewById(R.id.wind_units);
                     wind_units.setText(getResources().getString(R.string.wind_units_imperial));
                     updateWeather();
                 }
@@ -107,6 +157,11 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void tempClick(View v) {
+        Toast.makeText(mContext, "Updated!", Toast.LENGTH_SHORT).show();
+        updateWeather();
     }
 
 }
